ea058dcb77401929bbe40f252dbedb4362ea4552
Sufyaan <sufyaanmd182@gmail.com>
2026-07-03 18:50:40 +0100

    Release version 0.1.1

--- src/autofeature/__init__.py
+++ src/autofeature/__init__.py
@@ -7,7 +7,7 @@ from .categorical import SmartCategoricalEncoder
 from .leakage import LeakageDetector
 from .pipeline import AutoFeaturePipeline
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "AutoFeatureEngineer",
